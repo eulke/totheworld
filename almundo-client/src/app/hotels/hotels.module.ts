f9477ce8ad2a37a9134e435f2e64fa20fa5ca2c3
Esteban Ulke <eulke@oneloop.com.ar>
2019-12-21 15:47:00 -0300

    Rename folder

--- almundo-client/src/app/hotels/hotels.module.ts
+++ almundo-client/src/app/hotels/hotels.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HotelsListComponent } from './hotels-list/hotels-list.component';
 import { HotelCardComponent } from './hotel-card/hotel-card.component';
-import { HotelsComponent } from './hotels/hotels.component';
+import { HotelsComponent } from './hotels-layout/hotels.component';
 import { HotelsFilterComponent } from './hotels-filter/hotels-filter.component';
 import { HttpClientModule } from '@angular/common/http'
 import { AngularSvgIconModule } from 'angular-svg-icon';
